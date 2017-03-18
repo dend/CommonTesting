63cf8b83362f3531e0f3bafc156fc1f51eabadbf
Den Delimarsky <ddelimarsky@live.com>
2017-03-18 13:46:47 -0700

    Fixing the mdoc script.

--- scripts/mdocex.ps1
+++ scripts/mdocex.ps1
@@ -1,6 +1,11 @@
 ﻿$exePath = ($Env:BUILD_REPOSITORY_LOCALPATH + "\mdoc\mdoc.exe")
+Write-Output $exePath
+
 $azureLibs = ($Env:BUILD_REPOSITORY_LOCALPATH + "\nue-out")
+Write-Output $azureLibs
+
 $outputFolder = ($Env:BUILD_REPOSITORY_LOCALPATH + "\mdoc-output")
+Write-Output $outputFolder
 
 $individualPackages = Get-ChildItem -Path $azureLibs -Directory
 
@@ -9,14 +14,14 @@ foreach($package in $individualPackages)
 {
     if (-Not ($package.Name -Eq 'dependencies')){
         # Write-Output $package.FullName
-        New-Item $outputFolder\$package -Type Directory -force 
+        New-Item $outputFolder\$package -Type Directory -force
 
         $dlls = Get-ChildItem -Path $package.FullName -Filter *.dll
         foreach($dll in $dlls)
         {
             # Write-Output $dll.FullName
             $reflectionTarget = [io.path]::GetFileNameWithoutExtension($dll.FullName)
-            
+
             $docPath = ($package.FullName + "\" + $reflectionTarget + ".xml")
             $documentationXmlExists = Test-Path $docPath
 
