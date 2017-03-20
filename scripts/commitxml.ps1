213d7951df352783914cff23bfef32aa10e8fde9
Den Delimarsky <ddelimarsky@live.com>
2017-03-20 16:44:55 -0700

    Git configuration.

--- scripts/commitxml.ps1
+++ scripts/commitxml.ps1
@@ -1,11 +1,16 @@
 ls
 Get-ChildItem $srcFolder | Where {$_.PSIsContainer -and ($_ -match '^nue.*$')} | Remove-Item -Recurse
+Get-ChildItem $srcFolder | Where {$_.PSIsContainer -and ($_ -match '^nue\.zip*$')} | Remove-Item -Recurse
 Get-ChildItem $srcFolder | Where {$_.PSIsContainer -and ($_ -match '^_xml.*$')} | Remove-Item -Recurse
 Rename-Item -Path ($Env:BUILD_REPOSITORY_LOCALPATH +"\mdoc-output") -newName _xml
-Get-ChildItem $srcFolder | Where {$_.PSIsContainer -and ($_ -match '^_mdoc.*$')} | Remove-Item -Recurse
+Get-ChildItem $srcFolder | Where {$_.PSIsContainer -and ($_ -match '^mdoc.*$')} | Remove-Item -Recurse
+Get-ChildItem $srcFolder | Where {$_.PSIsContainer -and ($_ -match '^mdoc\.zip*$')} | Remove-Item -Recurse
 
 # Remove the warning, that otherwise breaks the build
 git config --global core.safecrlf false
+
+git config --global user.name ($GitHub.CustomUserName)
+git config --global user.email ($GitHub.CustomEmail)
 
 ls
 git add .
