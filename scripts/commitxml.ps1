3af18a6467ae7f04d11d52def57aa6d19e0b9c8a
Den Delimarsky <ddelimarsky@live.com>
2017-03-20 15:32:18 -0700

    XML File test.

--- scripts/commitxml.ps1
+++ scripts/commitxml.ps1
@@ -1,8 +1,8 @@
 ls
-Get-ChildItem $srcFolder | Where {$_.PSIsContainer -and ($_ -match '^nue.*$')} | Remove-Item -Recurse -WhatIf
-Get-ChildItem $srcFolder | Where {$_.PSIsContainer -and ($_ -match '^_xml.*$')} | Remove-Item -Recurse -WhatIf
+Get-ChildItem $srcFolder | Where {$_.PSIsContainer -and ($_ -match '^nue.*$')} | Remove-Item -Recurse
+Get-ChildItem $srcFolder | Where {$_.PSIsContainer -and ($_ -match '^_xml.*$')} | Remove-Item -Recurse
 Rename-Item -Path ($Env:BUILD_REPOSITORY_LOCALPATH +"\mdoc-output") -newName _xml
-Get-ChildItem $srcFolder | Where {$_.PSIsContainer -and ($_ -match '^_mdoc.*$')} | Remove-Item -Recurse -WhatIf
+Get-ChildItem $srcFolder | Where {$_.PSIsContainer -and ($_ -match '^_mdoc.*$')} | Remove-Item -Recurse
 
 ls
 git add .
