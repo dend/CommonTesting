95724ea204db48ea19b50e9317d9e4c2a2feb312
Den Delimarsky <ddelimarsky@live.com>
2017-03-20 20:18:26 -0700

    Fix script.

--- scripts/commitxml.ps1
+++ scripts/commitxml.ps1
@@ -1,19 +1,20 @@
-ls
-Get-ChildItem $srcFolder | Where {$_.PSIsContainer -and ($_ -match '^nue.*$')} | Remove-Item -Recurse
-Get-ChildItem $srcFolder | Where {$_.PSIsContainer -and ($_ -match '^nue\.zip*$')} | Remove-Item -Recurse
-Get-ChildItem $srcFolder | Where {$_.PSIsContainer -and ($_ -match '^_xml.*$')} | Remove-Item -Recurse
+Get-ChildItem
+
+Get-ChildItem $srcFolder | Where-Object {$_.PSIsContainer -and ($_ -match '^nue.*$')} | Remove-Item -Recurse
+Get-ChildItem $srcFolder | Where-Object {$_.PSIsContainer -and ($_ -match '^_xml.*$')} | Remove-Item -Recurse
 Rename-Item -Path ($Env:BUILD_REPOSITORY_LOCALPATH +"\mdoc-output") -newName _xml
-Get-ChildItem $srcFolder | Where {$_.PSIsContainer -and ($_ -match '^mdoc.*$')} | Remove-Item -Recurse
-Get-ChildItem $srcFolder | Where {$_.PSIsContainer -and ($_ -match '^mdoc\.zip*$')} | Remove-Item -Recurse
+Get-ChildItem $srcFolder | Where-Object {$_.PSIsContainer -and ($_ -match '^mdoc.*$')} | Remove-Item -Recurse
+Remove-Item mdoc.zip
+Remove-Item nue.zip
 
 # Remove the warning, that otherwise breaks the build
 git config --global core.safecrlf false
 
 git config --global user.name ($Env:GITHUB_CUSTOMUSERNAME)
 git config --global user.email ($Env:GITHUB_CUSTOMEMAIL)
-git commit --amend --reset-author
 
-ls
+Get-ChildItem
+
 git add .
 git commit -m ("Updating content based on build " + $Env:BUILD_BUILDNUMBER)
 git push origin HEAD:garbage
