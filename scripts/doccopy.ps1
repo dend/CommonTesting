2bbaa497f0649417b8ec557b9d6e5fc69ca19565
Den Delimarsky <ddelimarsky@live.com>
2017-03-21 15:09:20 -0700

    :fire: Adding doc scanner

---
+++ scripts/doccopy.ps1
@@ -0,0 +1,26 @@
+﻿$docPack = ($Env:BUILD_REPOSITORY_LOCALPATH + "\docpack")
+$azPack = ($Env:BUILD_REPOSITORY_LOCALPATH + "\nue-out")
+
+$xmlFiles = Get-ChildItem $docPack | Where-Object { $_.Extension -Eq ".xml" } | Foreach-Object {$_.Name}
+Write-Output $xmlFiles
+
+$existingPackages = Get-ChildItem $azPack | Where-Object { $_.Name -Ne "dependencies" }
+Write-Output $existingPackages
+
+foreach($package in $existingPackages)
+{
+    $pacFile = Get-ChildItem ($package.FullName) | Where-Object { $_.Extension -Eq ".dll" } | Select-Object -First 1
+    $fileName = [System.IO.Path]::GetFileNameWithoutExtension($pacFile)
+
+    Write-Output ("Trying to find docs for "+ $fileName)
+
+    $docFileTarget = ($fileName + ".xml");
+    Write-Output $docFileTarget
+
+    if ($xmlFiles.Contains($docFileTarget))
+    {
+        Write-Output "We have docs!"
+        Write-Output $package.FullName
+        Copy-Item ($docPack + "\" + $docFileTarget) ($package.FullName + "\" + $docFileTarget)
+    }
+}
