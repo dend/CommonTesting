3e27d61c26ef75af99ac0388407cb00fb68cde6b
Den Delimarsky <ddelimarsky@live.com>
2017-03-25 17:30:24 -0700

    Significant script mods.

--- scripts/mdocex.ps1
+++ scripts/mdocex.ps1
@@ -6,6 +6,7 @@ Write-Output $azureLibs
 
 $outputFolder = ($Env:BUILD_REPOSITORY_LOCALPATH + "\mdoc-output")
 Write-Output $outputFolder
+New-Item ($outputFolder + "\docs") -Type Directory -force
 
 $individualPackages = Get-ChildItem -Path $azureLibs -Directory
 
@@ -13,30 +14,71 @@ Write-Output "Current packages:"
 foreach($package in $individualPackages)
 {
     if (-Not ($package.Name -Eq 'dependencies')){
-        # Write-Output $package.FullName
-        New-Item $outputFolder\$package -Type Directory -force
+        $finalPackageOutput = ($outputFolder + "\" + $package + "\" + $package)
+        $packageDocOutput = ($outputFolder + "\docs\" + $package)
+
+        # Default the dependency folder.
+        $finalDependencyOutput = ($azureLibs + "\dependencies\" + $package)
+
+        # Create a new folder for the package, and related subfolders.
+        # We treat individual Azure packages as if those are "frameworks" on their own.
+        New-Item ($packageDocOutput) -Type Directory -force
+
+        New-Item ($outputFolder + "\" + $package) -Type Directory -force
+        New-Item ($finalPackageOutput) -Type Directory -force
 
         $dlls = Get-ChildItem -Path $package.FullName -Filter *.dll
+        $dependencies = Get-ChildItem -Path ($azureLibs + "\dependencies\" + $package) -Filter *.dll
+        
+        # Copy all dependencies locally into the package folder.
+        if ($dependencies.count -gt 0)
+        {
+            $finalDependencyOutput = ($outputFolder + "\" + $package + "\" + $package + "\dependencies\" + $package)
+
+            New-Item ($outputFolder + "\" + $package + "\" + $package + "\dependencies") -Type Directory -force
+            New-Item ($finalDependencyOutput) -Type Directory -force
+
+            foreach($dependency in dependencies)
+            {
+                $depFileName = [io.path]::GetFileName($dependency.FullName)
+                Copy-Item $dependency.FullName ($finalDependencyOutput + "\" + $depFileName)
+            }
+        }
+
         foreach($dll in $dlls)
         {
             # Write-Output $dll.FullName
-            $reflectionTarget = [io.path]::GetFileNameWithoutExtension($dll.FullName)
+            $dllName = [io.path]::GetFileName($dll.FullName)
+            $packageFullName = [io.path]::GetFileNameWithoutExtension($dll.FullName)
 
-            $docPath = ($package.FullName + "\" + $reflectionTarget + ".xml")
+            Copy-Item $dll.FullName ($finalPackageOutput + "\" + $dllName)
+
+            # When we ran NUE, we might've already obtained the XML that is shipped with the package.
+            # If that is the case, we want to generate the output for the DLL from it, but we need to
+            # test the path first.
+            $docPath = ($package.FullName + "\" + $packageFullName + ".xml")
+            $newDocPath = ($finalPackageOutput + "\" + $packageFullName + ".xml")
             $documentationXmlExists = Test-Path $docPath
-
-            $resolutionPath = ($azureLibs + "\dependencies\" + $package.Name)
-            Write-Output ("Resolving from " + $resolutionPath)
 
             if ($documentationXmlExists)
             {
                 Write-Output "Found XML documentation file!"
-                & $exePath update -i $docPath -o ($outputFolder) $dll.FullName -L $resolutionPath --use-docid
+                Copy-Item $docPath $newDocPath
+                
+                # Run the standard import of the existing doc file.
+                & $exePath update -i $newDocPath -o ($packageDocOutput) $dll.FullName -L $finalDependencyOutput --use-docid
+
+                # Now run the framework tooling.
+                & $exePath fx-bootstrap $finalPackageOutput
+                New-Item ($finalPackageOutput + "\temp") -Type Directory -force
+                & $exePath update -fw $finalPackageOutput -o ($finalPackageOutput + "\temp") --use-docid
+                Copy-Item ($finalPackageOutput + "\temp\FrameworksIndex") ($packageDocOutput + "\FrameworksIndex")
             }
             else
             {
                 Write-Output "There is no XML documentation file."
-                & $exePath update -o ($outputFolder) $dll.FullName -L $resolutionPath --use-docid
+                & $exePath fx-bootstrap $finalPackageOutput
+                & $exePath update -fw $finalPackageOutput -o ($packageDocOutput) --use-docid
             }
         }
     }
