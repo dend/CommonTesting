1e65cbe1c14a9ea92cd4a890797ff39f15b8e1b3
Den Delimarsky <ddelimarsky@live.com>
2017-05-27 18:05:13 -0700

    :package: Script tweaks.

--- scripts/demoscript.ps1
+++ scripts/demoscript.ps1
@@ -7,29 +7,26 @@ Write-Output $libraries
 $outputFolder = ($Env:BUILD_REPOSITORY_LOCALPATH + "\mdoc-output")
 Write-Output $outputFolder
 
+$dependencyPath = ($Env:BUILD_REPOSITORY_LOCALPATH + "\nue-out\dependencies\jsonnet-10.0.2")
 
 $dlls = Get-ChildItem -Path ($libraries + "\*") -Include *.dll
 foreach($dll in $dlls)
 {
-    # Write-Output $dll.FullName
     $reflectionTarget = [io.path]::GetFileNameWithoutExtension($dll.FullName)
 
     $docPath = ($libraries + "\" + $reflectionTarget + ".xml")
     $documentationXmlExists = Test-Path $docPath
 
-    #$resolutionPath = ($libraries + "\dependencies\" + $package.Name)
-    #Write-Output ("Resolving from " + $resolutionPath)
-
     if ($documentationXmlExists)
     {
         Write-Output "Found XML documentation file!"
         Write-Output $dll.FullName
-        & $exePath update -i $docPath -o ($outputFolder) $dll.FullName --use-docid
+        & $exePath update -i $docPath -o ($outputFolder) -L ($dependencyPath) $dll.FullName --use-docid
     }
     else
     {
         Write-Output "There is no XML documentation file."
         Write-Output $dll.FullName
-        & $exePath update -o ($outputFolder) $dll.FullName --use-docid
+        & $exePath update -o ($outputFolder) -L ($dependencyPath) $dll.FullName --use-docid
     }
 }
