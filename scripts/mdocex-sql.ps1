5cd497ababfd7da91fc2ccb8f68f7de869143b8f
Den Delimarsky <ddelimarsky@live.com>
2017-06-02 12:38:11 -0700

    :package: Yet another fix.

--- scripts/mdocex-sql.ps1
+++ scripts/mdocex-sql.ps1
@@ -17,7 +17,7 @@ foreach($dll in $dlls)
     $docPath = ($libraries + "\" + $reflectionTarget + ".xml")
     $documentationXmlExists = Test-Path $docPath
 
-    $resolutionPath = ($libraries + "\dependencies\sqlserver-2017")
+    $resolutionPath = ($Env:BUILD_REPOSITORY_LOCALPATH + "\sql\dependencies\sqlserver-2017")
     #Write-Output ("Resolving from " + $resolutionPath)
 
     if ($documentationXmlExists)
