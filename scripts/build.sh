becd4d618b4fb5d02923691261b63788bd098c69
Den Delimarsky <ddelimarsky@live.com>
2018-06-30 23:44:53 -0700

    :fire: Fix folder path

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,6 +15,6 @@ mkdir $BUILD_SOURCESDIRECTORY/_publish
 wget -O $BUILD_SOURCESDIRECTORY/_dl/docfx.zip "https://github.com/dotnet/docfx/releases/download/v2.37/docfx.zip"
 unzip $BUILD_SOURCESDIRECTORY/_dl/docfx.zip -d $BUILD_SOURCESDIRECTORY/_bin/docfx
 
-cd $BUILD_SOURCESDIRECTORY/_publish
-mono $BUILD_SOURCESDIRECTORY/_bin/docfx/docfx.exe init
+cd $BUILD_SOURCESDIRECTORY/$DOCSFOLDER
+mono $BUILD_SOURCESDIRECTORY/_bin/docfx/docfx.exe
 
