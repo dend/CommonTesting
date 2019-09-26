2b4251a1bffb2f5dcb9c42a7801b4fe8b2284e3a
Den Delimarsky <1389609+dend@users.noreply.github.com>
2019-09-25 22:02:43 -0700

    Update with some random code.

---
+++ dotnet-projects/nullable-casting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace stack_overflow
+{
+    public enum Car
+    {
+        Fast = 1,
+        Slow = 2,
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car? car = null;
+
+            int? num1 = CarToInt(car);  // A
+            int? num2 = (int)car;      // B
+
+            Console.WriteLine($"{num1} {num2}");
+        }
+
+        public static int? CarToInt(Car? car)
+        {
+            if (car == null)
+                return null;
+
+            return (int)car.Value;
+        }
+    }
+}
---
+++ dotnet-projects/xml-parsing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace xml_parsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductsFilteredBySubCategory("test.xml");
+        }
+
+        public static void ProductsFilteredBySubCategory(string path)
+        {
+            XDocument document = XDocument.Load(path);
+            //var elements = document.Descendants().Where(node => (string)node.Attribute("id") == "SUB1000");
+            
+            var elements = document.Descendants("subcategory")
+                            .Where(i => (string)i.Attribute("id") == "SUB1000")
+                            .Select(i => i.Parent.Parent);
+
+            foreach(var element in elements)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+    }
+}
