089912c8f9e6c004b82236b549a08f322888cac3
Den Delimarsky <1389609+dend@users.noreply.github.com>
2019-09-26 21:56:40 -0700

    JSON parsing example in C#

---
+++ dotnet-projects/json-restructure/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace json_restructure
+{
+    class Entity
+    {
+        [JsonProperty("id")]
+        public string id;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = "[{\"id\":\"1127889\"},{\"id\":\"1075442\"},{\"id\":\"1201544\"}]";
+            var workingObject = JsonConvert.DeserializeObject<List<Entity>>(json);
+
+            foreach(var t in workingObject)
+            {
+                Console.WriteLine(t.id);
+            }
+
+            var idList = new { id = (from c in workingObject select c.id).ToArray()};
+
+            var outputString = JsonConvert.SerializeObject(idList);
+
+            Console.WriteLine(outputString);
+        }
+    }
+}
