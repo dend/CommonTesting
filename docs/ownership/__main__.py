8dd573c1639a3c5a11885eaaa03d2cb8453e8d10
Den Delimarsky <1389609+dend@users.noreply.github.com>
2020-01-17 13:50:13 -0800

    Update __main__.py

--- docs/ownership/__main__.py
+++ docs/ownership/__main__.py
@@ -1,6 +1,9 @@
 import argparse
 from sys import argv
 
+def get_owner(url):
+
+
 PARSER = argparse.ArgumentParser(description='ownership - version 0.1')
 SUB_PARSERS = PARSER.add_subparsers(dest="commands_parser")
 MAKE_PARSER = SUB_PARSERS.add_parser('find')
@@ -11,3 +14,24 @@ MAKE_PARSER.add_argument('--out', type=str, metavar='O',
                          help='Output path for the generated ownership file.')
 
 ARGS = PARSER.parse_args()
+
+if len(argv) <= 1:
+    print('[error] No command line arguments supplied to the CLI. Terminating.')
+else:
+    if ARGS.commands_parser == 'find':
+        print('[info] Looking for ownership of docs.microsoft.com URLs...')
+
+        if ARGS.list is not None and ARGS.out is not None:
+            url_list = []
+            owner_map = []
+
+            with open(ARGS.list) as txt_file:
+                url_list = txt_file.read().splitlines()
+
+            if url_list is not None:
+                for (url in url_list):
+                    owner_map.append(get_owner(url))
+            else:
+                print('[error] No URLs were read from the file.')
+    else:
+        print('[error] The command you specified (or not) could not be used with the ownership tool.')
